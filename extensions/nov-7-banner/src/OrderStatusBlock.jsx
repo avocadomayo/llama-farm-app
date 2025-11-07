19d71578a6e7fcfd7dd0cca5630a8a596a80a1e1
Wendy Liu <wendy.liu@shopify.com>
2025-11-07 13:03:58 -0500

    Update nov-7-banner extension code

--- extensions/nov-7-banner/src/OrderStatusBlock.jsx
+++ extensions/nov-7-banner/src/OrderStatusBlock.jsx
@@ -1,25 +1,13 @@
 import {
-  BlockStack,
   reactExtension,
-  TextBlock,
   Banner,
-  useApi,
 } from "@shopify/ui-extensions-react/customer-account";
 
 export default reactExtension(
   "customer-account.order-index.block.render",
-  () => <PromotionBanner />
+  () => <Extension />
 );
 
-function PromotionBanner() {
-  console.log("PromotionBanner");
-  const { i18n } = useApi();
-
-  return (
-    <Banner>
-      <BlockStack inlineAlignment="center">
-        <TextBlock>This is rendered on remote ui</TextBlock>
-      </BlockStack>
-    </Banner>
-  );
+function Extension() {
+  return <Banner>This is rendered on remote ui</Banner>;
 }
